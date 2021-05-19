fd8dae62678be70cb2868d7d872d25047a5fea14
jkang <junmin050@gmail.com>
2021-05-19 17:06:39 +0900

    Updated Overall Skeleton Structure

---
+++ android/app/src/main/kotlin/com/example/giveandtake/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.giveandtake
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'main.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: buildAppBar(context),
+      drawer: buildDrawer(context),
+      bottomNavigationBar: buildNavBar(context),
+      body: Consumer<ApplicationState>(
+        builder: (context, appState, _) => SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // 로그인한 유저 정보 불러오기 (FirebaseAuth 사용)
+              children: [
+                Text('Name: ' + FirebaseAuth.instance.currentUser.displayName),
+                Text('Email: ' + FirebaseAuth.instance.currentUser.email),
+                Text('UID: ' + FirebaseAuth.instance.currentUser.uid),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Builder Widget for AppBar
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      title: Text('Give'),
+      backgroundColor: Colors.cyan,
+      leading: IconButton(
+        icon: Icon(
+          Icons.menu_rounded,
+          semanticLabel: 'menu',
+        ),
+        onPressed: () =>
+            _scaffoldKey.currentState.openDrawer(), // Open drawer on pressed
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.search,
+            semanticLabel: 'search',
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  // Builder Widget for Bottom Navigation Bar
+  BottomNavigationBar buildNavBar(BuildContext context) {
+    return BottomNavigationBar(
+      selectedItemColor: Colors.cyan,
+      type: BottomNavigationBarType.fixed,
+      // Tap actions for each tab
+      onTap: (index) => {},
+      currentIndex: 0,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Give',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.card_giftcard),
+          label: 'Take',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.messenger),
+          label: 'Chat',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'MyPage',
+        )
+      ],
+    );
+  }
+
+  // Builder Widget for Drawer
+  Drawer buildDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  '-Drawer-',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.cyan,
+            ),
+          ),
+          ListTile(
+            title: Text('Home'),
+            // - The Menu Icons should be placed in the leading position
+            leading: Icon(
+              Icons.home,
+            ),
+            onTap: () {
+              // - Each menu should be navigated by Named Routes
+            },
+          ),
+          ListTile(
+            title: Text('Search'),
+            leading: Icon(
+              Icons.search,
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text('My Page'),
+            leading: Icon(
+              Icons.person,
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text('Settings'),
+            leading: Icon(
+              Icons.settings,
+            ),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
