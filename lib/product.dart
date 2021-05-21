734a71bbd2d0a91b5838d533e4f80130615395d5
dpseul <21800251@handong.edu>
2021-05-21 23:22:03 +0900

    Merge branch 'master' of C:\Users\SAMSUNG\dev\mad_capstone with conflicts.

--- lib/product.dart
+++ lib/product.dart
@@ -3,26 +3,20 @@ import 'package:flutter/foundation.dart';
 
 class Product {
   Product({
-    @required this.category,
-    @required this.title,
-    @required this.content,
+    @required this.name,
+    @required this.price,
+    @required this.description,
     @required this.created,
     @required this.modified,
     @required this.uid,
     @required this.id,
-    @required this.like,
-    @required this.mark,
-    @required this.comments,
   });
 
-  final String category;
-  final String title;
-  final String content;
+  final String name;
+  final int price;
+  final String description;
   final Timestamp created;
   final Timestamp modified;
   final String uid;
   final String id;
-  final int like;
-  final bool mark;
-  final int comments;
 }
