2d04ace6dd8c41ccf5b6360dfd97af68badda18f
chansik <chlckstlr0428@gmail.com>
2021-08-22 01:21:19 +0900

    최초 로그인시에만 닉네임 설정, 그 후에는 자동 로그인

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -2,10 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:giveandtake/model/user_chat.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'home.dart';
 
 
@@ -20,113 +16,15 @@ class SignUp extends StatefulWidget {
 }
 
 class SignUpState extends State<SignUp> {
-  final GoogleSignIn googleSignIn = GoogleSignIn();
-  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-  SharedPreferences prefs;
-  bool isLoading = false;
-  bool isLoggedIn = false;
-  User currentUser;
 
-  @override
-  void initState() {
-    super.initState();
-    isSignedIn();
-  }
-
-  void isSignedIn() async {
-    setState(() {
-      isLoading = true;
-    });
-
-    prefs = await SharedPreferences.getInstance();
-
-    isLoggedIn = await googleSignIn.isSignedIn();
-    if (isLoggedIn && prefs?.getString('id') != null) {
-      await Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => HomePage(currentUserId: prefs.getString('id') ?? "")),
-      );
-    }
-
-    setState(() {
-      isLoading = false;
-    });
-  }
+  CollectionReference users =
+  FirebaseFirestore.instance.collection('users');
 
 
-  Future<UserCredential> handleSignIn() async {
-    prefs = await SharedPreferences.getInstance();
-
-    setState(() {
-      isLoading = true;
-      isLoggedIn = true;
-    });
-
-    var googleUser = await googleSignIn.signIn();
-    if (googleUser != null) {
-      var googleAuth = await googleUser.authentication;
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-
-      var firebaseUser = (await firebaseAuth.signInWithCredential(credential)).user;
-
-      if (firebaseUser != null) {
-        // Check is already sign up
-        final QuerySnapshot result =
-        await FirebaseFirestore.instance.collection('UserName').where('id', isEqualTo: firebaseUser.uid).get();
-        final List<DocumentSnapshot> documents = result.docs;
-        if (documents.isEmpty) {
-          // Update data to server if new user
-          await FirebaseFirestore.instance.collection('UserName').doc(firebaseUser.uid).set({
-            'uid': firebaseUser.uid,
-            'email': firebaseUser.email,
-            'photoUrl': firebaseUser.photoURL,
-            'username': textEditingController1.text,
-            'created': FieldValue.serverTimestamp(),
-            'isLogged' : isLoggedIn,
-          });
-          currentUser = firebaseUser;
-          await prefs?.setString('uid', currentUser.uid);
-          await prefs?.setString('username', currentUser.displayName ?? "");
-          await prefs?.setString('photoUrl', currentUser.photoURL ?? "");
-        } else {
-          var documentSnapshot = documents[0];
-          var userChat = UserChat.fromDocument(documentSnapshot);
-          // Write data to local
-          await prefs?.setString('uid', userChat.id);
-          await prefs?.setString('username', userChat.nickname);
-          await prefs?.setString('photoUrl', userChat.photoUrl);
-          await prefs?.setString('aboutMe', userChat.aboutMe);
-        }
-        await Fluttertoast.showToast(msg: "Sign in success");
-        setState(() {
-          isLoading = false;
-        });
-
-       // Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(currentUserId: firebaseUser.uid)));
-      } else {
-        await Fluttertoast.showToast(msg: "Sign in fail");
-        setState(() {
-          isLoading = false;
-        });
-      }
-    } else {
-      await Fluttertoast.showToast(msg: "Can not init google sign in");
-      setState(() {
-        isLoading = false;
-      });
-    }
-  }
 
 
   @override
   Widget build(BuildContext context) {
-    //final uid = FirebaseAuth.instance.currentUser.uid;
-    //final email =  FirebaseAuth.instance.currentUser.email;
-    //bool isLogged = false;
-   // CollectionReference signup = FirebaseFirestore.instance.collection("UserName");
 
 
     return Material(
@@ -156,42 +54,6 @@ class SignUpState extends State<SignUp> {
               ),
 
 
-              ElevatedButton.icon(
-                label: Text('Sign in with Google'),
-                icon: Icon(Icons.android),
-                onPressed: ()   async {
-                  // Sign in with Google account,
-                  // Traditional style (then, catchError) used here
-
-
-
-                  await handleSignIn().then((value) {
-                    print('User: ' + value.user.displayName);
-                   /*
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Authenticated\n',
-
-                        )));
-
-                    */
-                    // Display User Info with SnackBar
-                    // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    //   content: Text('Welcome, ' + value.user.displayName + '!'),
-                    //   behavior: SnackBarBehavior.fixed,
-                    //   duration: Duration(seconds: 1),
-                    // ));
-
-
-                  });
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.cyan,
-                  onPrimary: Colors.white,
-                  shadowColor: Colors.black,
-                  elevation: 3,
-                ),
-              ),
-
               Divider(
                 color: Colors.white,
                 height: 20,
@@ -205,21 +67,27 @@ class SignUpState extends State<SignUp> {
                     ElevatedButton(
                       onPressed: () async {
                         print("click actionbutton");
-                       // isLogged = true;
 
-                        if(isLoggedIn){
-                          textEditingController1.clear();
+                        await FirebaseFirestore.instance.collection('users')
+                            .doc(FirebaseAuth.instance.currentUser.uid)
+                            .set({
+
+                          'username': FirebaseAuth.instance.currentUser.displayName,
+                          'photoUrl': FirebaseAuth.instance.currentUser.photoURL,
+                          'id': FirebaseAuth.instance.currentUser.uid,
+                          'createdAt': DateTime
+                              .now()
+                              .millisecondsSinceEpoch
+                              .toString(),
+                          'nickname': textEditingController1.text,
+
+                        }).then((value) async {
                           await Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => HomePage(currentUserId: prefs.getString('id') ?? "")));
+                                  builder: (context) => HomePage()));
+                        }).catchError((error) => print('Error: $error'));
 
-
-
-                        }
-                        else{
-                          await Fluttertoast.showToast(msg: "You should Login with Google");
-                        }
 
 
                       },
@@ -245,21 +113,5 @@ class SignUpState extends State<SignUp> {
   }
 }
 
-/*
-    Future <void> createUser (String uid, String email, String username){
-
-
-      FirebaseFirestore.instance.collection("UserName")
-          .add(
-          {
-            uid: uid,
-            email: email,
-            username: username,
-          }
-      );
-
-    }
-
- */
 
 
