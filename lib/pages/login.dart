84353ade08ac37d1d733e57acb10f32a7836ad08
chansik <chlckstlr0428@gmail.com>
2021-09-16 21:20:33 +0900

    한동 구글 계정 로그인 주의사항 추가

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -102,7 +102,7 @@ class _LoginPageState extends State<LoginPage> {
 
 
         }
-        await Fluttertoast.showToast(msg: "Sign in success");
+        await Fluttertoast.showToast(msg: 'Sign in success');
         setState(() {
           isLoading = false;
         });
@@ -114,13 +114,13 @@ class _LoginPageState extends State<LoginPage> {
 
       }
       else {
-        await Fluttertoast.showToast(msg: "Sign in fail");
+        await Fluttertoast.showToast(msg: 'Sign in fail');
         setState(() {
           isLoading = false;
         });
       }
     } else {
-      await Fluttertoast.showToast(msg: "Can not init google sign in");
+      await Fluttertoast.showToast(msg: 'Can not init google sign in');
       setState(() {
         isLoading = false;
       });
@@ -129,8 +129,57 @@ class _LoginPageState extends State<LoginPage> {
     }
     @override
     Widget build(BuildContext context) {
+/*
+      final snackBar = SnackBar(
+        content: const Text('한동 구글 계정으로 로그인 해야 합니다'),
+        action: SnackBarAction(
+          label: 'dismiss',
+          onPressed: () {
+            // Some code to undo the change.
+          },
+        ),
+      );
+
+
+ */
+//ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+      void _showDialog() {
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Row(
+                children: [
+                  Icon(Icons.add_alert, color: Colors.amberAccent),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text('주의사항'),
+                  ),
+                ],
+              ),
+              content: SingleChildScrollView(child:Text('반드시 한동대학교 구글 계정으로 로그인하셔야 합니다!')),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () async {
+                    Navigator.pop(context);
+                    await handleSignIn();
+
+                  },
+                  child: Text('한동 구글 계정으로 로그인하기'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+
+
+
       return Scaffold(
-        body: SafeArea(
+        body:
+        SafeArea(
           child: ListView(
             padding: EdgeInsets.symmetric(horizontal: 24.0),
             children: <Widget>[
@@ -139,7 +188,8 @@ class _LoginPageState extends State<LoginPage> {
                 children: <Widget>[
                   Container(
                       height: 50,
-                      child: Image.asset('assets/hgu.png', fit: BoxFit.fill)),
+                      child: Image.asset('assets/hgu.png', fit: BoxFit.fill)
+                  ),
                   Text('MAD Final Project'),
                 ],
               ),
@@ -152,9 +202,11 @@ class _LoginPageState extends State<LoginPage> {
                   onPressed: () async {
                     // Sign in with Google account,
                     // Traditional style (then, catchError) used here
-                    await handleSignIn();
+                    // ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    _showDialog();
 
 
+                    //await handleSignIn();
                   },
                   style: ElevatedButton.styleFrom(
                     primary: Colors.cyan,
@@ -167,7 +219,34 @@ class _LoginPageState extends State<LoginPage> {
             ],
           ),
         ),
+
       );
     }
   }
+
+
+  ///banner 사용법 (snackbar 와 달리, banner 는 dismiss 버튼을 눌러야 사라진다.
+///필독 사항이 있을 때 사용하면 좋을 것 같다
+///flutter 2.5 버전부터 사용 가능
+  /*
+  Center(
+          child: ElevatedButton(
+            child: const Text('Show MaterialBanner'),
+            onPressed: () => ScaffoldMessenger.of(context).showMaterialBanner(
+              MaterialBanner(
+                content: const Text('Hello, I am a Material Banner'),
+                leading: const Icon(Icons.info),
+                backgroundColor: Colors.yellow,
+                actions: [
+                  TextButton(
+                    child: const Text('Dismiss'),
+                    onPressed: () => ScaffoldMessenger.of(context)
+                        .hideCurrentMaterialBanner(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+   */
 
